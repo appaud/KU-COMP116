4d12e493467b9c8bd8dce742b29b566a47e54724
appaud <aayushpaudyal43@gmail.com>
2026-08-08 16:46:34 +0545

    object created

--- Q2.cpp
+++ Q2.cpp
@@ -18,3 +18,10 @@ class circle{
         return 2*3.14*radius;
     }
 };
+
+int main(){
+    circle c1(7);
+    std::cout<<"area of circle is "<<c1.getarea()<<std::endl;
+    std::cout<<"perimeter of circle is "<<c1.getperi()<<std::endl;
+
+}
