6e984917ae75f411753ce9bd06e4f36e4fa2ad6b
appaud <aayushpaudyal43@gmail.com>
2026-08-08 16:39:34 +0545

    Q2 solved

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,16 @@
 //hello this is 2nd question//
+
+#include<iostream>
+
+class circle{
+    private:
+    double radius;
+
+    public:
+    circle(){}
+    circle(double radius):radius(radius){}
+
+    void setsize(double radius);
+    double getarea();
+    double getperi();
+};
