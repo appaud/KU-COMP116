fd54845f727104e7236f21a4f32bf36896c67e64
appaud <aayushpaudyal43@gmail.com>
2026-08-09 18:23:45 +0545

    main defined

--- q3.cpp
+++ q3.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 
 class point{
     private:
@@ -9,12 +10,19 @@ class point{
     point(){}
     point(int x,int y):x(x),y(y){}
 
-    float getdistance(){
-        
+    float getdistance(point& p){
+        int xd=x-p.x;
+        int yd=y-p.y;
+
+        return sqrt(pow(xd,2)+pow(yd,2));
+
     }
 
 };
 
 int main(){
+    point p1(2,3);
+    point p2(3,4);
+    std::cout<<"distance between points is"<<p1.getdistance(p2);
 
 }
