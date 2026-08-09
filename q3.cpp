a52b85d0da8a4a20046fc89b6e8009d288dbeb0b
appaud <aayushpaudyal43@gmail.com>
2026-08-09 18:20:47 +0545

    class defined

--- q3.cpp
+++ q3.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 
 class point{
+    private:
+    int x;
+    int y;
+
+    public:
+    point(){}
+    point(int x,int y):x(x),y(y){}
+
+    float getdistance(){
+        
+    }
 
 };
 
 int main(){
-    
+
 }
