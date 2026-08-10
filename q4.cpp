17b1cf69e3fefa7eb6870459b22d2ad2f7cea62b
appaud <aayushpaudyal43@gmail.com>
2026-08-10 18:15:06 +0545

    function called

--- q4.cpp
+++ q4.cpp
@@ -7,3 +7,16 @@ template<typename t> void area(t a,t b){
     std::cout<<"area of reactangle is"<<area;
 }
 
+int main(){
+    std::cout<<"enter length and breadth of rectangle in integer"<<std::endl;
+    int a,b;
+    float c,d;
+    std::cin>>a>>b;
+    area(a,b);
+
+    std::cout<<"enter length and breadth of rectangle in float"<<std::endl;
+    std::cin>>c>>d;
+    area(c,d);
+
+
+}
