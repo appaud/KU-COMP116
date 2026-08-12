ea91960193a11a0c1d33cf2bb7e9fd61fcc0c237
appaud <aayushpaudyal43@gmail.com>
2026-08-12 17:58:49 +0545

    new feature

--- q6.cpp
+++ q6.cpp
@@ -9,10 +9,17 @@ class point{
     point(int x,int y):x(x),y(y){}
 
     friend std::ostream& operator<<(std::ostream &os,const point& p);
+    friend std::istream& operator>>(std::istream &os,const point& p);
+    
+
 };
 
 std::ostream& operator<<(std::ostream &os,const point& p){
     return os<<"("<<p.x<<","<<p.y<<")";
+}
+
+std::istream& operator>>(std::istream &os,const point& p){
+    return os>>p.x>>p.y;
 }
 
 int main(){
