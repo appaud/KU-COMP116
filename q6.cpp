0bf14c008544956b9017266c7dd36c93b38afb93
appaud <aayushpaudyal43@gmail.com>
2026-08-12 18:00:30 +0545

    debug and test

--- q6.cpp
+++ q6.cpp
@@ -9,7 +9,7 @@ class point{
     point(int x,int y):x(x),y(y){}
 
     friend std::ostream& operator<<(std::ostream &os,const point& p);
-    friend std::istream& operator>>(std::istream &os,const point& p);
+    friend std::istream& operator>>(std::istream &os, point& p);
     
 
 };
@@ -18,8 +18,8 @@ std::ostream& operator<<(std::ostream &os,const point& p){
     return os<<"("<<p.x<<","<<p.y<<")";
 }
 
-std::istream& operator>>(std::istream &os,const point& p){
-    return os>>p.x>>p.y;
+std::istream& operator>>(std::istream &is, point& p) {
+    return is >> p.x >> p.y;
 }
 
 int main(){
