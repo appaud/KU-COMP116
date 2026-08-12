24018bd24b0c814c65f29e611f5e13e3c57f6e7d
appaud <aayushpaudyal43@gmail.com>
2026-08-12 17:53:19 +0545

    operator overloaded

---
+++ q6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+class point{
+    private:
+    int x;
+    int y;
+    public:
+    point(){}
+    point(int x,int y):x(x),y(y){}
+
+    friend std::ostream& operator<<(std::ostream &os,const point& p);
+};
+
+std::ostream& operator<<(std::ostream &os,const point& p){
+    return os<<"("<<p.x<<","<<p.y<<")";
+}
+
